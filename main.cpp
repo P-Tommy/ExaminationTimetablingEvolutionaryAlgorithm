bdc174ce9a6b2f5eb33726ce65cc92d5ead73481
Roberto Poo <xaro@poo.cl>
2013-06-12 22:43:17 -0400

    Add conflicts matrix

--- main.cpp
+++ main.cpp
@@ -1,22 +1,29 @@
 #include <iostream>
 #include <fstream>
-#include <map>
 #include <algorithm>
 #include <iterator>
+#include <vector>
 
 int main ()
 {
-  std::map<int, int> exams; // Saves the number of students per exam
 
-  std::ifstream myfile("ear-f-83.crs");
+  std::ifstream exams_file("ear-f-83.crs");
 
   // Count the newlines to get the total of exams
-  myfile.unsetf(std::ios_base::skipws); // Don't skip newlines
+  exams_file.unsetf(std::ios_base::skipws); // Don't skip newlines
   unsigned total_exams = std::count(
-    std::istream_iterator<char>(myfile),
+    std::istream_iterator<char>(exams_file),
     std::istream_iterator<char>(),
     '\n');
 
-  std::cout << total_exams;
+  std::vector< std::vector<int> > conflicts;
+
+  conflicts.resize(total_exams);
+
+  // Initializes the matrix
+  for (int i = 0; i < total_exams; ++i)
+    conflicts[i].resize(total_exams);
+
+  std::cout << conflicts[10][1];
   return 0;
 }
