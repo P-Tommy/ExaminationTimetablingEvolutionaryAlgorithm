b3ec9c4ce1168f67ef43ab8fbe370a96da6444f3
Roberto Poo <xaro@poo.cl>
2013-06-13 00:02:07 -0400

    Filled the conflict matrix

--- main.cpp
+++ main.cpp
@@ -3,11 +3,15 @@
 #include <algorithm>
 #include <iterator>
 #include <vector>
+#include <sstream>
+#include <boost/tokenizer.hpp>
 
-int main ()
-{
+#define PROBLEM_CRS "ear-f-83.crs"
+#define PROBLEM_STU "ear-f-83.stu"
 
-  std::ifstream exams_file("ear-f-83.crs");
+int main () {
+
+  std::ifstream exams_file(PROBLEM_CRS);
 
   // Count the newlines to get the total of exams
   exams_file.unsetf(std::ios_base::skipws); // Don't skip newlines
@@ -24,6 +28,32 @@ int main ()
   for (int i = 0; i < total_exams; ++i)
     conflicts[i].resize(total_exams);
 
-  std::cout << conflicts[10][1];
+  // Read the students file to fill the conflicts matrix
+  std::ifstream infile(PROBLEM_STU);
+
+  boost::char_separator<char> sep(" "); // Exams separated by spaces
+
+  // We parse every line of the file, filling the conflict matrix
+  std::string line;
+  while (std::getline(infile, line)) {
+
+    // We tokenize the line and mark as conflicts every pair of exams
+    // that appears in the same line
+    std::istringstream iss(line);
+    boost::tokenizer<boost::char_separator<char> > tokens(line, sep);
+    if (tokens.begin() != tokens.end()) { // Check if there is more than one exam
+      for(boost::tokenizer< boost::char_separator<char> >::iterator exam = tokens.begin(); exam != tokens.end(); ++exam) {
+        boost::tokenizer< boost::char_separator<char> >::iterator other_exam = exam;
+
+        // We iterate on all the remaining exams in the line, after the current one
+        // and mark the couple (current, next) and (next, current) as conflicted
+        for(other_exam++; other_exam != tokens.end(); ++other_exam) {
+          conflicts[atoi((*exam).c_str()) - 1][atoi((*other_exam).c_str()) - 1] = 1;
+          conflicts[atoi((*other_exam).c_str()) - 1][atoi((*exam).c_str()) - 1] = 1;
+        }
+      }
+    }
+  }
+
   return 0;
 }
