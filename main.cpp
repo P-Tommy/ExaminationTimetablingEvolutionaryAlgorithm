a98791ab77b82420d4b4151c883ed495d3252a62
Roberto Poo <xaro@poo.cl>
2013-06-16 00:33:36 -0400

    Format

--- main.cpp
+++ main.cpp
@@ -26,8 +26,10 @@ unsigned total_exams; // Number of exams
 std::vector< std::vector<int> > conflicts;
 
 // Prints the conflict matrix
-void print_conflicts() {
-  for (std::vector< std::vector<int> >::iterator i = conflicts.begin() ; i != conflicts.end(); ++i) {
+void print_conflicts()
+{
+  for (std::vector< std::vector<int> >::iterator i = conflicts.begin() ; i != conflicts.end(); ++i)
+  {
     std::cout << "\n";
     for (std::vector<int>::iterator conflict = (*i).begin() ; conflict != (*i).end(); ++conflict)
       std::cout << ' ' << *conflict;
@@ -49,7 +51,8 @@ public:
   int exams; // Total exams assigned to this Timeslot
   int id; // An identifier for the timeslot
 
-  Timeslot(int _id) {
+  Timeslot(int _id)
+  {
     exams = 0;
     id = _id;
   }
@@ -63,7 +66,8 @@ private:
   static int used_timeslots; // Counts the total timeslots that are occupied
 
   // Calculates the solution aptitude (and sets it)
-  void calculate_aptitude() {
+  void calculate_aptitude()
+  {
     aptitude = 0;
 
     for (int i = 0; i < total_exams; ++i)
@@ -77,22 +81,23 @@ private:
 
           // Depending on the distance, we assign a different penalization
           // and it gets amplified by the number of students with the same conflict
-          switch (distance) {
-            case 0:
-              aptitude += W0 * conflicts.at(i).at(j);
-              break;
-            case 1:
-              aptitude += W1 * conflicts.at(i).at(j);
-              break;
-            case 2:
-              aptitude += W2 * conflicts.at(i).at(j);
-              break;
-            case 3:
-              aptitude += W3 * conflicts.at(i).at(j);
-              break;
-            default:
-              aptitude += W4 * conflicts.at(i).at(j);
-              break;
+          switch (distance)
+          {
+          case 0:
+            aptitude += W0 * conflicts.at(i).at(j);
+            break;
+          case 1:
+            aptitude += W1 * conflicts.at(i).at(j);
+            break;
+          case 2:
+            aptitude += W2 * conflicts.at(i).at(j);
+            break;
+          case 3:
+            aptitude += W3 * conflicts.at(i).at(j);
+            break;
+          default:
+            aptitude += W4 * conflicts.at(i).at(j);
+            break;
           }
         }
       }
@@ -107,16 +112,19 @@ public:
   std::vector<Timeslot*> genotype;
 
   // Generates a random solution
-  Solution() {
+  Solution()
+  {
     genotype.resize(total_exams);
     timeslots.resize(total_exams);
 
     int i = 0;
-    for (std::vector<Timeslot*>::iterator timeslot = timeslots.begin(); timeslot != timeslots.end(); ++timeslot, ++i) {
+    for (std::vector<Timeslot*>::iterator timeslot = timeslots.begin(); timeslot != timeslots.end(); ++timeslot, ++i)
+    {
       *timeslot = new Timeslot(i);
     }
 
-    for (std::vector<Timeslot*>::iterator exam = genotype.begin() ; exam != genotype.end(); ++exam) {
+    for (std::vector<Timeslot*>::iterator exam = genotype.begin() ; exam != genotype.end(); ++exam)
+    {
       // Fill every exam with it's own timeslot
       *exam = timeslots[rand() % total_exams];
       (*exam)->exams++; // Increment the number of exams assigned to the timeslot
@@ -126,8 +134,10 @@ public:
   }
 
   // Prints the solution in stdout
-  void print() {
-    for (std::vector<Timeslot*>::iterator exam = genotype.begin() ; exam != genotype.end(); ++exam) {
+  void print()
+  {
+    for (std::vector<Timeslot*>::iterator exam = genotype.begin() ; exam != genotype.end(); ++exam)
+    {
       std::cout << (*exam)->id << " ";
     }
     std::cout << " | " << aptitude << "\n";
@@ -137,7 +147,8 @@ int Solution::used_timeslots = 0;
 
 
 std::vector< Solution* > population;
-int main () {
+int main ()
+{
   srand(time(NULL)); // Sets a random seed
 
   total_exams = get_total_exams();
@@ -146,7 +157,8 @@ int main () {
   print_conflicts();
   population.resize(total_exams);
 
-  for (std::vector< Solution* >::iterator solution = population.begin() ; solution != population.end(); ++solution) {
+  for (std::vector< Solution* >::iterator solution = population.begin() ; solution != population.end(); ++solution)
+  {
     *solution = new Solution();
   }
   print_pop();
@@ -154,13 +166,16 @@ int main () {
   return 0;
 }
 
-void print_pop() {
-  for (std::vector< Solution* >::iterator solution = population.begin() ; solution != population.end(); ++solution) {
+void print_pop()
+{
+  for (std::vector< Solution* >::iterator solution = population.begin() ; solution != population.end(); ++solution)
+  {
     (*solution)->print();
   }
 }
 
-void fill_conflicts(int total_exams) {
+void fill_conflicts(int total_exams)
+{
   conflicts.resize(total_exams);
 
   // Initializes the matrix
@@ -174,19 +189,23 @@ void fill_conflicts(int total_exams) {
 
   // We parse every line of the file, filling the conflict matrix
   std::string line;
-  while (std::getline(infile, line)) {
+  while (std::getline(infile, line))
+  {
 
     // We tokenize the line and mark as conflicts every pair of exams
     // that appears in the same line
     std::istringstream iss(line);
     boost::tokenizer<boost::char_separator<char> > tokens(line, sep);
-    if (tokens.begin() != tokens.end()) { // Check if there is more than one exam
-      for(boost::tokenizer< boost::char_separator<char> >::iterator exam = tokens.begin(); exam != tokens.end(); ++exam) {
+    if (tokens.begin() != tokens.end())   // Check if there is more than one exam
+    {
+      for(boost::tokenizer< boost::char_separator<char> >::iterator exam = tokens.begin(); exam != tokens.end(); ++exam)
+      {
         boost::tokenizer< boost::char_separator<char> >::iterator other_exam = exam;
 
         // We iterate on all the remaining exams in the line, after the current one
         // and mark the couple (current, next) and (next, current) as conflicted
-        for(other_exam++; other_exam != tokens.end(); ++other_exam) {
+        for(other_exam++; other_exam != tokens.end(); ++other_exam)
+        {
           conflicts[atoi((*exam).c_str()) - 1][atoi((*other_exam).c_str()) - 1]++;
           conflicts[atoi((*other_exam).c_str()) - 1][atoi((*exam).c_str()) - 1]++;
         }
@@ -195,15 +214,16 @@ void fill_conflicts(int total_exams) {
   }
 }
 
-int get_total_exams() {
+int get_total_exams()
+{
   std::ifstream exams_file(PROBLEM_CRS);
 
   // Count the newlines to get the total of exams
   exams_file.unsetf(std::ios_base::skipws); // Don't skip newlines
   unsigned total_exams = std::count(
-    std::istream_iterator<char>(exams_file),
-    std::istream_iterator<char>(),
-    '\n');
+                           std::istream_iterator<char>(exams_file),
+                           std::istream_iterator<char>(),
+                           '\n');
 
   return total_exams;
 }
